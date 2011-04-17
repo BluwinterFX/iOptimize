b85fd3bc2c5cdc19447ac385771f3f14a1bcdd2e
Connor Harries <admin@bluwinter.co.uk>
2011-04-17 15:22:10 +0100

    Simple Tool To Optimize Png Files For iPhone Viewing

---
+++ iOptimize.bat
@@ -0,0 +1,19 @@
+@echo off
+title iOptimize
+color f0
+echo iOptimize
+set /P in=Drag Input File Here: 
+stub.exe %in% output.png
+echo.
+echo 10%%
+echo 17%%
+echo 29%%
+echo 35%%
+echo 57%%
+echo 73%%
+echo 81%%
+echo 95%%
+echo 100%%
+echo.
+echo Optimized PNG Saved As output.png
+pause >nul
